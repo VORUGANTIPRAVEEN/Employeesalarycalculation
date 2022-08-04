5a4518d4b0e54931767e402bc2c06b18544c8852
Name is Raju <75732731+VORUGANTIPRAVEEN@users.noreply.github.com>
2022-08-04 15:46:32 +0530

    first one
    java one

---
+++ Maincls.java
@@ -0,0 +1,17 @@
+package projectone;
+
+import java.util.Scanner;
+
+public class Maincls {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the employee  id");
+        int emp_id=sc.nextInt();
+        System.out.println("Enter the employee Name ");
+        String emp_name=sc.next();
+        
+        
+	}
+
+}
---
+++ empgetset.java
@@ -0,0 +1,68 @@
+package projectone;
+public class empgetset {
+	private String EmployeeName;
+	private int EmployeeId;
+	private double annualsalary;
+	private double monthsalary;
+	private double HRA;
+	private double DA;
+	private double GS;
+	private double incometax;
+    private double netsalary;
+	public double getMonthsalary() {
+		return monthsalary;
+	}
+	public void setMonthsalary(double monthsalary) {
+		this.monthsalary = monthsalary;
+	}
+	public double getHRA() {
+		return HRA;
+	}
+	public void setHRA(double hRA) {
+		HRA = hRA;
+	}
+	public double getDA() {
+		return DA;
+	}
+	public void setDA(double dA) {
+		DA = dA;
+	}
+	public double getGS() {
+		return GS;
+	}
+	public void setGS(double gS) {
+		GS = gS;
+	}
+	public double getIncometax() {
+		return incometax;
+	}
+	public void setIncometax(double incometax) {
+		this.incometax = incometax;
+	}
+	public double getNetsalary() {
+		return netsalary;
+	}
+	public void setNetsalary(double netsalary) {
+		this.netsalary = netsalary;
+	}
+	public String getEmployeeName() {
+		return EmployeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		EmployeeName = employeeName;
+	}
+	public int getEmployeeId() {
+		return EmployeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		EmployeeId = employeeId;
+	}
+	public double getAnnualsalary() {
+		return annualsalary;
+	}
+	public void setAnnualsalary(double annualsalary) {
+		this.annualsalary = annualsalary;
+	}
+	
+
+}
